e74ca664bf151cf96b41aba9b549c50d864d700d
Giacomo <giacomo@debugsrl.it>
2021-08-10 22:33:39 +0200

    root commit

---
+++ runthis-env.sh
@@ -0,0 +1,8 @@
+# run this script to setup the environment needed to build examples
+
+export INSTALL_DIR=~/opt/symbiflow
+export FPGA_FAM="xc7"
+export PATH="$INSTALL_DIR/$FPGA_FAM/install/bin:$PATH";
+source "$INSTALL_DIR/$FPGA_FAM/conda/etc/profile.d/conda.sh"
+conda activate $FPGA_FAM
+
