798eac1bbaa18c0b8c1d922420a4fc0016a6ed59
singds <giacomodalsasso@outlook.com>
2021-08-13 14:40:53 +0200

    better organization of cpu tests

--- riscv/cpu_test.v
+++ riscv/cpu_test.v
@@ -2,12 +2,10 @@
 module cpu_test;
 
     reg clk;
-    wire [31:0] inst_addr;
-    wire [31:0] inst_data;
+    wire [31:0] inst_addr; // unused
+    reg [31:0] inst_data;
     
-    inst_mem InstMem (inst_addr, inst_data);
-    cpu Cpu (clk, inst_addr, inst_data);
-
+    cpu Cpu (clk, inst_addr, inst_data); 
 
     reg [31:0] k; // variable for cycle
     initial begin
@@ -15,43 +13,51 @@ module cpu_test;
         $display("instructions sequence");
         
         clk = 0;
-        for (k = 0; k < 2; k++)
-        begin
-            #1 $display("inst_addr = %h, inst_data = %h", inst_addr, inst_data);
-            cycle;
-        end
+        inst_data = 0;
 
-        cycle;
-        assert (Cpu.xreg[1] == 32'h34) $display("ok: addi x1, x0, 0x34");
+        testNop ( );
+        testAddi ( );
 
         $finish(0);
     end
 
-    task cycle;
+    task clkCycle;
     begin
         #1 clk = 1;
         #1 clk = 0;
     end
+    endtask
 
-endtask
-
-endmodule
-
-module inst_mem
-(
-    input [31:0] address, // input address
-    output [31:0] data // output data
-);
-    localparam MEM_SIZE = 256;
-
-    reg [31:0] mem [0:MEM_SIZE-1];
-
-    assign data = mem[address[31:2]];
-
-    initial begin
-        mem['h0000] <= 32'h00000013; // nop
-        mem['h0001] <= 32'h00000013; // nop
-        mem['h0002] <= 32'h03400093; // addi x1, x0, 0x34
+    task exeInst;
+        input [31:0] instruction;
+    begin
+        inst_data = instruction;
+        clkCycle;
     end
+    endtask
+
+    // Test NOP operation
+    task testNop;
+    begin
+        assert (Cpu.pc == 32'h0);
+
+        exeInst (32'h00000013); // nop
+        assert (Cpu.pc == 32'h4);
+        else $display("pc=%h", Cpu.pc);
+
+        $display("ok: nop");
+    end
+    endtask
+
+    // Test ADDI operation
+    task testAddi;
+    begin
+        exeInst (32'h03400093); // addi x1, x0, 0x34
+        assert (Cpu.xreg[1] == 32'h34);
+
+        $display("ok: addi");
+    end
+    endtask
+
 endmodule
 
