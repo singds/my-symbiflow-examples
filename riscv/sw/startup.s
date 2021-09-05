de514ae0a661cf4433e2c0fb18c9fad7b1251987
singds <giacomodalsasso@outlook.com>
2021-09-05 09:36:52 +0200

    add linker relaxation, global pointer and small sections informations

--- riscv/sw/startup.s
+++ riscv/sw/startup.s
@@ -1,6 +1,33 @@
 # this is a comment
-# this only load the right stack pointer and jumps to main
-# the startup section contains the first instruction executed by the core
+
+# register's alternative names
+# x0 = zero
+# x1 = ra (return address)
+# x2 = sp (stack pointer)
+# x3 = gp (global pointer)
+# x4 = tp (thread pointer)
+# x5-7 = t0-2 (temporary registers)
+# x8 = s0 / fp (saved register / frame pointer)
+# x9 = s1 (saved register)
+# x10-11 = a0-1 (function arguments / return values)
+# x12-17 = a2-7 (function arguments)
+# x18-27 = s2-11 (saved registers)
+# x28-31 = t3-6 (temporary registers)
+
+# The startup section contains the first instruction executed by the core.
+# This section has the following tasks:
+# - If linker relaxation is used (__global_pointer$ symbol is defined in the linker
+#   script), initialize the gp register with __global_pointer$ address.
+#   gp register is not initialized in this startup so linker relaxation must
+#   be disabled: __global_pointer$ must not be defined in the linker script.
+#   For more informations about linker relaxation and gp see the linker script.
+#   Note that relaxation must be disabled while setting gp, otherwise
+#   "la gp, __global_pointer$" is relaxed by the linker in "mv gp, gp".
+#   See (.option norelax)
+# - Initialize (.bss) memory section with zero values.
+# - Initialize (.data) memory section with initial values stored in persistent storage.
+# - Initialize the the stack pointer.
+# - Jump to main.
 
 .section .startup
 # this is the first instruction executed
